e017e148ae8098088e2f0caa089186f3ffb91fea
Poornajith <chiranthapoornajith@gmail.com>
2025-01-14 11:55:44 +0530

    project filter workig

---
+++ src/Components/WebDev/Filter.jsx
@@ -0,0 +1,27 @@
+// Filter.js
+import React, { useState } from 'react';
+
+const categories = ['All', 'Design', 'Design and Development', 'Mobile App Design'];
+
+const Filter = ({ onFilterChange }) => {
+    const [selectedCategory, setSelectedCategory] = useState('All');
+
+    const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+        onFilterChange(event.target.value);
+    };
+
+    return (
+        <div>
+            <select value={selectedCategory} onChange={handleCategoryChange}>
+                {categories.map((category) => (
+                    <option key={category} value={category}>
+                        {category}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default Filter;
--- src/Components/WebDev/WebDevHome.jsx
+++ src/Components/WebDev/WebDevHome.jsx
@@ -2,10 +2,17 @@ import NavBar from "../NavBar";
 import {Container} from "react-bootstrap";
 import Slider from "./Slider";
 import Gallery from "./Gallery";
-import MaintenanceScreen from "../MaintenanceScreen";
-import {projects} from "./Projects";
+// import MaintenanceScreen from "../MaintenanceScreen";
+import {projects,filterProjects} from "./Projects";
+import Filter from './Filter';
+import {useState} from "react";
 
 export default function WebDevHome(){
+
+    const [filteredProjects, setFilteredProjects] = useState(projects);
+    const handleFilterChange = (selectedCategory) => {
+        setFilteredProjects(filterProjects(projects, selectedCategory));
+    };
 
     return(
         <div>
@@ -13,10 +20,10 @@ export default function WebDevHome(){
             <Slider></Slider>
             <Container>
                 {/*<MaintenanceScreen></MaintenanceScreen>*/}
-                {projects.map((project) => (
+                <Filter onFilterChange={handleFilterChange} />
+                {filteredProjects.map((project) => (
                     <Gallery key={project.title} image={project.images} text={project} />
                 ))}
-
             </Container>
         </div>
     )
